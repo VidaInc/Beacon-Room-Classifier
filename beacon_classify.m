a2317109e8c01518dc7da777502a62b1cdc813f7
Aaron Nwabuoku <aerisvulpe@gmail.com>
2015-08-05 19:11:41 -0400

    Include raw RSSI values as features.

--- beacon_classify.m
+++ beacon_classify.m
@@ -3,13 +3,13 @@ clear ; close all; clc
 
 % Load Data
 load('data');
-X = data(:, 1:10); y = data(:, 11);
+y = data(:, 13);
 %X = horzcat(ones(size(X, 1), 1), data(:,1:10));
 
 % Add Polynomial Features
 % Note that mapFeature also adds a column of ones for us, so the intercept
 % term is handled
-X = horzcat(mapFeature(data(:, 1), data(:, 2), data(:, 3)), data(: , 4:10));
+X = horzcat(mapFeature(data(:, 1), data(:, 2), data(:, 3)), data(: , 4:12));
 
 % Initialize fitting parameters
 initial_theta = zeros(size(X, 2), 1);
