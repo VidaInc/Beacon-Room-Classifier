acd579c21a4a401917d26c6631cf95b4085bc86f
Aaron Nwabuoku <aerisvulpe@gmail.com>
2015-08-05 21:16:15 -0400

    Minor change to sigmoid function.

--- sigmoid.m
+++ sigmoid.m
@@ -2,7 +2,7 @@ function g = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-g = 1./(1+exp(1).^-z);
+g = 1./(1 + exp(-z));
 
 % =============================================================
 
